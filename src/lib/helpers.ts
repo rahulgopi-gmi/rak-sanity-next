7c9891ccbbf8d1038789b6de2c15f304e5feb10e
rahulgopi-gmi <rahulgopi@globalmediainsight.agency>
2025-12-26 13:18:22 +0530

    new build changes

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -3,5 +3,5 @@ export function normalizeArray<T>(value?: T | T[]): T[] {
   return Array.isArray(value) ? value : [value];
 }
 
-export const formatPrice = (price) =>
+export const formatPrice = (price:any) =>
   price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
