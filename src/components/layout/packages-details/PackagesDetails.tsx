63df970575d056315821312683d0f4f498713ec4
rahulgopi-gmi <rahulgopi@globalmediainsight.agency>
2026-01-07 17:52:34 +0530

    package page changes

--- src/components/layout/packages-details/PackagesDetails.tsx
+++ src/components/layout/packages-details/PackagesDetails.tsx
@@ -113,7 +113,7 @@ export default function PackagesDetails(
                                         1200: { slidesPerView: 4 },
                                         1400: { slidesPerView: 4, spaceBetween: 15 },
                                     }}
-                                    className="mySwiper max-w-full mx-auto"
+                                    className="package-swiper max-w-full mx-auto"
                                 >
                                     {
                                             sortedPackageItems.map((p: PackageType, index: number) => {
@@ -123,7 +123,7 @@ export default function PackagesDetails(
 
                                                 return(
                                                     <SwiperSlide key={p._id}>
-                                                        <div className="rounded-[20px] border border-[rgba(255,255,255,0.14)] bg-[linear-gradient(129deg,rgba(255,255,255,0.19)_8.15%,rgba(255,255,255,0.04)_93.89%)] backdrop-blur-[21.5px] min-h-[560px] sm:min-h-[610px] p-[23px] text-left max-w-[287px] mx-auto sm:max-w-none sm:mx-0">
+                                                        <div className="swiper-slide-inner rounded-[20px] border border-[rgba(255,255,255,0.14)] bg-[linear-gradient(129deg,rgba(255,255,255,0.19)_8.15%,rgba(255,255,255,0.04)_93.89%)] backdrop-blur-[21.5px] min-h-[560px] sm:min-h-[610px] p-[23px] text-left max-w-[287px] mx-auto sm:max-w-none sm:mx-0">
                                                             <h4 className="text-white text-[16px]! font-sans font-normal uppercase mb-[25px]">
                                                                 {p.title}
                                                             </h4>
